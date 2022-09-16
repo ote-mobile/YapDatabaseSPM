3491c789ebd3298dc22d4e87509a735ec90a0b3f
Ruslan Samsonov <rsamsonov@devexperts.com>
2022-09-16 12:29:05 +0300

    binaries for tag

---
+++ export/iOS/YapDatabaseSPM.xcframework/ios-arm64_x86_64-simulator/YapDatabaseSPM.framework/Headers/YapDatabaseSPM.h
@@ -0,0 +1,13 @@
+//
+//  YapDatabaseSPM.h
+//  YapDatabase
+//
+//  Created by rsamsonov on 15.09.2022.
+//  Copyright © 2022 Deusty LLC. All rights reserved.
+//
+
+#ifndef YapDatabaseSPM_h
+#define YapDatabaseSPM_h
+
+
+#endif /* YapDatabaseSPM_h */
